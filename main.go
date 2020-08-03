5284fe1a5648fd8a36f334e26ab419b87714559b
李小明 <likeming@jasgroup.cn>
2020-08-03 17:06:31 +0800

    创建

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb_son "lilu.red/go-grpc-test/son"
+)
+
+const (
+	port = ":40000"
+)
+
+type server struct{
+	pb_son.UnimplementedSonServiceServer
+}
+
+//实现Insert接口
+func (s *server) Insert(ctx context.Context, in *pb_son.Info) (*pb_son.Info, error) {
+	log.Println("增加" + in.Name)
+	return &pb_son.Info{Name: in.Name + "已经增加"}, nil
+}
+
+func main() {
+	log.Println("服务启动")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	//注册服务
+	pb_son.RegisterSonServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ test/main_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"context"
+	"log"
+	"testing"
+	"time"
+
+	"google.golang.org/grpc"
+	pb_son "lilu.red/go-grpc-test/son"
+)
+
+// TestTime 测试时间
+func TestTime(t *testing.T) {
+	log.Println(time.Now().Format("2006-01-02 15:04:05-07"))
+}
+
+func TestGrpc(t *testing.T) {
+	conn, err := grpc.Dial("localhost:40000", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb_son.NewSonServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Insert(ctx, &pb_son.Info{Name: "小明"})
+	if err != nil {
+		log.Fatalf("调用失败: %v", err)
+	}
+	log.Printf("回复: %s", r.GetName())
+}
